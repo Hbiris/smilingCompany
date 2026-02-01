7f5c513574876793aaa190eb07ede0493d8310cb
yumozi <xueeric1223@gmail.com>
2026-02-01 15:20:20 -0500

    Change camera

--- SmilingCompany/Assets/MonsterScript/GuardManager.cs
+++ SmilingCompany/Assets/MonsterScript/GuardManager.cs
@@ -8,6 +8,11 @@ public class GameManager : MonoBehaviour
     public void Die(string reason)
     {
         Debug.Log($"PLAYER DIED: {reason}");
+        if (player == null)
+            Debug.LogError("[GameManager] player is NULL!");
+        if (spawnPoint == null)
+            Debug.LogError("[GameManager] spawnPoint is NULL!");
+
         if (player != null && spawnPoint != null)
             player.position = spawnPoint.position;
     }
